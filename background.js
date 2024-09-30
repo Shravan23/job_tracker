0f62372055d5bf2fe026535a6e343cf6ef848efe
Shravan Kumar Bachu <bachs01@pfw.edu>
2024-09-29 20:43:36 -0400

    Setup the Initial Extension with OAuth to read Emails

---
+++ background.js
@@ -0,0 +1,12 @@
+chrome.runtime.onInstalled.addListener(() => {
+    console.log('Extension installed.');
+  });
+  
+  chrome.identity.onSignInChanged.addListener((account, signedIn) => {
+    if (signedIn) {
+      console.log(`User ${account.email} signed in.`);
+    } else {
+      console.log(`User ${account.email} signed out.`);
+    }
+  });
+  
